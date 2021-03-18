edf2b00e26ab369c043fd3d2193c3d1dedd33f0b
MaiBlom <mai.blommen@gmail.com>
2021-03-19 00:55:26 +0100

    Implement Pokedex and Pokeroll commands. Initialize Pokemon dir

--- index.js
+++ index.js
@@ -4,60 +4,74 @@ const dataFolder = './DATA/';
 var commands = require('./pCommands.js');
 var fs = require('fs');
 
-exports.pokemons = pokemons;
-exports.pokeInts = dataArr
-exports.pokemonPool = pokemonPool;
-exports.userFiles = userFiles;
-
 const bot = new Discord.Client({ 
-  partials: ['MESSAGE', 'CHANNEL'],
-  ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] }
+    partials: ['MESSAGE', 'CHANNEL'],
+    ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] }
 });
 
 var pokemons = [];
-var dataArr = [];
+var pokemonInts = [];
 var pokemonPool = 0;
 var userFiles = [];
 bot.on('ready', () => {
-  fs.readdir(dataFolder, (err, files) => {
+    fs.readdir(dataFolder, (err, files) => {
     if (err) { console.log(err) }
-    files.forEach(file => {
-      if (file != 'pokemons.txt')
+        files.forEach(file => { 
+        if (file != 'pokemons.txt')
         userFiles.push(file);
     });
-  });
+});
 
-  fs.readFile("./DATA/pokemons.txt", "utf-8", (err, data) => {
+fs.readFile("./DATA/pokemons.txt", "utf-8", (err, data) => {
     if (err) { console.log(err) }
-    dataArr = data.split(/(?:;| |\r|\n|,)+/);
-    for (i = 0; i < dataArr.length - 1; i++) {
-      pokemons[i] = dataArr.splice(i, 1);
-      pokemonPool += parseInt(dataArr[i]);
-    }
-  });
+        pokemonInts = data.split(/(?:;| |\r|\n|,)+/);
+        for (i = 0; i < pokemonInts.length - 1; i++) {
+            pokemons[i] = pokemonInts.splice(i, 1);
+            pokemonPool += parseInt(pokemonInts[i]);
+        }
+    });
 });
 
 bot.on('message', (msg) => {
-  if (msg.member.user.bot) return;
-  if (msg.content.charAt(0) != '%') return;
-  let msgContent = msg.content.split(" ");
-  let msgChannel = msg.channel.id;
-  let msgMemberID = msg.member.id;
-  let msgMemberNickName = msg.author.tag;
+    if (msg.member.user.bot) return;
+    if (msg.content.charAt(0) != '%') return;
+    let msgContent = msg.content.split(" ");
+    let msgChannel = msg.channel.id;
+    let msgMemberID = msg.member.id;
+    let msgUserObj = msg.author;
+    let memberPokemons = [];
 
-  if(msgContent.length === 1 && msgContent[0].charAt(0) === '%') {
-    msgContent[0] = msgContent[0].substring(1);
-    switch(msgContent[0]) {
-      case "pr":
-        commands.pokeRoll(bot, msgChannel, msgMemberID);
-        break;
-      case "pd":
-        commands.pokedex(bot, msgChannel, msgMemberID);
-        break;
+    if(!fs.existsSync(`./DATA/${msgMemberID}.txt`)) {
+        fs.writeFile(`./DATA/${msgMemberID}.txt`, '', 'utf-8', err => { console.log(err) });
+        bot.channels.cache.get(msgChannel).send(`Welcome **${msgUserObj.username}**! Do \`\`%pd\`\` to check your Pokédex, and \`\`%pr\`\` to roll for Pokémon!`);
+        return;
     }
-  }
 
-  console.log(`Message: "${msgContent}" with length ${msgContent.length} \nNickname: ${msgMemberNickName} \nUser ID: ${msgMemberID} \nMessage Channel ID: ${msgChannel}`);
-})
+    fs.readFile(`./DATA/${msgMemberID}.txt`, 'utf-8', (err, data) => {
+        if(err) console.log(err);
+        memberPokemons = data.split(/(?:;| |\r|\n)+/);
+        memberPokemons.splice(memberPokemons.length - 1, 1);
+
+        if(msgContent.length === 1 && msgContent[0].charAt(0) === '%') {
+            msgContent[0] = msgContent[0].substring(1);
+
+            switch(msgContent[0]) {
+                case "pr":
+                    commands.pokeRoll(bot, msgChannel, msgMemberID, msgUserObj, pokemons, pokemonInts, pokemonPool);
+                    break;
+                case "pd":
+                    commands.pokedex(bot, msgChannel, msgMemberID, msgUserObj, memberPokemons);
+                    break;
+            }   
+        }
+    });
+
+    // console.log(`Message: "${msgContent}" with length ${msgContent.length} \nNickname: ${msgMemberNickName} \nUser ID: ${msgMemberID} \nMessage Channel ID: ${msgChannel}`);
+});
+
+bot.on('messageReactionAdd', async (reaction, user) => {
+    if(user.bot) return;
+    // let msg = reaction.message;
+});
 
 bot.login(BOTTOKEN);
--- pCommands.js
+++ pCommands.js
@@ -1,20 +1,77 @@
-var { pokemons, pokeInts, pokemonPool, userFiles } = require('./index.js');
+const Discord = require('discord.js');
 var fs = require("fs");
+//const { Console } = require('console');
 
 module.exports = {
-    pokeRoll: function(bot, channel, msgMemberID) {
+    pokeRoll: function(bot, channel, msgMemberID, msgUserObj, pokemons, pokemonInts, pokemonPool) {
+        let rollNum = getRandomInt(1, pokemonPool);
+
+        let curPos = 0;
+        var rollPokemon;
+
+        for(i = 0; i < pokemons.length - 1; i++) {
+            if(rollNum >= curPos && rollNum <= curPos + parseInt(pokemonInts[i])) {
+                rollPokemon = pokemons[i].join();
+                if(getRandomInt(1, 1000) >= 999) rollPokemon = '$' + rollPokemon;
+                break;
+            }
+            curPos += parseInt(pokemonInts[i]);
+        }
         
+        fs.readFile(`./DATA/${msgMemberID}.txt`, 'utf-8', (err, data) => {
+            if(err) console.log(err);
+            if(data.includes(rollPokemon)) {
+                var indexOfNum = data.indexOf(rollPokemon) + rollPokemon.length + 1;
+
+                var numOfPokemon = parseInt(data.charAt(indexOfNum) + data.charAt(indexOfNum + 1) + data.charAt(indexOfNum + 2));
+                numOfPokemon++;
+                numOfPokemon = numOfPokemon.toString();
+
+                if(numOfPokemon.length === 1) numOfPokemon = '00' + numOfPokemon;
+                else if(numOfPokemon.length === 2) numOfPokemon = '0' + numOfPokemon;
+
+                data = setCharAt(data, indexOfNum, numOfPokemon.charAt(0));
+                data = setCharAt(data, indexOfNum + 1, numOfPokemon.charAt(1));
+                data = setCharAt(data, indexOfNum + 2, numOfPokemon.charAt(2));
+
+                fs.writeFile(`./DATA/${msgMemberID}.txt`, data, 'utf-8', err => { if(err) console.log(err); })
+            } else {
+                data += rollPokemon + ',001;';
+                fs.writeFile(`./DATA/${msgMemberID}.txt`, data, 'utf-8', err => { if(err) console.log(err); })
+            }
+        });
+
+        bot.channels.cache.get(channel).send(`**${msgUserObj.username}** caught **${rollPokemon}**!`);
     },
     
-    // msgMemberID formatting.
-    pokedex: function(bot, channel, msgMemberID) {
-        var memberPokemons = [];
-        fs.readFile(`./DATA/${msgMemberID}.txt`, "utf-8", (err, data) => {
-            if (err) { console.log(err) }
-            memberPokemons = data.split(/(?:;| |\r|\n)+/);
-            console.log(`${msgMemberID}'s pokemons: \n${memberPokemons}`);
-            
-        });
+    pokedex: function(bot, channel, msgMemberID, msgUserObj, memberPokemons) {
+        let fieldArray = [];
+
+        if(memberPokemons.length == 0) 
+            fieldArray.push('Empty Pokedex');
+        else
+            for(i = 0; i < 10 && i < memberPokemons.length; i++) {
+                let pokemonShiny = false;
+                if(memberPokemons[i].charAt(0) === '$') {
+                    pokemonShiny = true;
+                    memberPokemons[i] = memberPokemons[i].substring(1);
+                }
+                let pokemonType = memberPokemons[i].split(',')[0];
+                let pokemonAmount = parseInt(memberPokemons[i].split(',')[1]);
+                if(pokemonShiny)
+                    fieldArray.push(`${pokemonAmount}x :sparkles:${pokemonType}`);
+                else
+                    fieldArray.push(`${pokemonAmount}x ${pokemonType}`);
+            }
+
+        const pokedexEmbed = new Discord.MessageEmbed()
+            .setColor('#8f1255')
+            .setTitle(`${msgUserObj.username}\'s pokedex`)
+            .setThumbnail(msgUserObj.avatarURL())
+            .setDescription(fieldArray)
+            .setTimestamp();
+
+        bot.channels.cache.get(channel).send(pokedexEmbed).then(s => { s.react('◀'); s.react('▶'); });
     }
 };
 
@@ -31,4 +88,10 @@ function getRandomArbitrary(min, max) {
 // Returns random Integer between the specified values. Greater than or equal to min | Lower than max.
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + Math.ceil(min)));
+}
+
+// Replaces a particular char at an index in a String.
+function setCharAt(str, index, chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0, index) + chr + str.substring(index + 1);
 }
